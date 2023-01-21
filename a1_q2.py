ebeb236a97077bfa368446c4b87eed086e5f3b1e
Sakshi2107 <123103899+Sakshi2107@users.noreply.github.com>
2023-01-21 20:21:46 +0530

    Submitting Q2 of Assignment 1

---
+++ a1_q2.py
@@ -0,0 +1,11 @@
+n=int(input('Enter the marks: '))
+if n>90:
+    print('Excellent')
+elif n>80 and n<=90:
+    print('Good')
+elif n>70 and n<=80:
+    print('Fair')
+elif n>60 and n<=70:
+    print('Meets expectations')
+else:
+    print('Below par')s
